6797272cdfcf11764d024bec295823d160fc9886
Mohammed Mizban <Mohammed.mizban97@gmail.com>
2021-04-16 09:17:40 +0300

    run the app

--- app/src/main/java/com/ibareq/the_chance_week6/LoginUtil.kt
+++ app/src/main/java/com/ibareq/the_chance_week6/LoginUtil.kt
@@ -3,11 +3,11 @@ package com.ibareq.the_chance_week6
 object LoginUtil {
 
     fun login(email: String, password: String){
-        TODO("Not yet implemented")
+        //TODO("Not yet implemented")
     }
 
     fun logout(){
-        TODO("Not yet implemented")
+        //TODO("Not yet implemented")
     }
 
 }
